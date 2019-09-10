ddb06e5e8280cc4a943bf4e046fa2db24016ebaa
daniel.liu <daniel.liu@xteamstudio.com>
2019-09-10 13:21:58 +0800

    add githook

---
+++ src/test/java/com/xteamstudio/exam/order_system/TestRedisServer.java
@@ -0,0 +1,21 @@
+package com.xteamstudio.exam.order_system;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+import redis.embedded.RedisServer;
+
+@Component
+public class TestRedisServer implements InitializingBean, DisposableBean {
+    private RedisServer redisServer = new RedisServer(6379);
+
+    @Override
+    public void destroy() throws Exception {
+        redisServer.stop();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        redisServer.start();
+    }
+}
